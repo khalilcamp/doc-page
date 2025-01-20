be965b1e556945f9de8c0014968312f81a9713ef
Khalil Camargo <kacamargopacker@gmail.com>
2025-01-20 11:06:13 -0300

    fix:ReactNF

--- src/assets/Grafana_icon.tsx
+++ src/assets/Grafana_icon.tsx
@@ -1,6 +1,6 @@
-import * as React from "react"
+// import * as React from "react"
 
-const SvgComponent = (props) => (
+const SvgComponent = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     xmlSpace="preserve"
